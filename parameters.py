b0a09198b862f73c7b799fd766baf128a94aa311
Hopsakee <jdejong@posteo.nl>
2025-01-11 22:19:34 +0100

    Small changes to parameters.py

--- parameters.py
+++ parameters.py
@@ -20,22 +20,24 @@ if is_wsl:
     # Get Windows user profile path
     import subprocess
     windows_user = subprocess.check_output(['cmd.exe', '/c', 'echo %USERNAME%'], text=True).strip()
+else:
+    windows_user = "dummy"
 
-    # 
-    # User parameters
-    #
-    # Location of input and output files
-    # TODO: make us of ~ possible in setting of path
-    FABRIC_PATTERNS_FOLDER="/home/jelle/.config/fabric/patterns"
-    OBSIDIAN_OUTPUT_FOLDER="/mnt/c/Obsidian/BrainCave/Extra/textgenerator/templates/fabric"
-    OBSIDIAN_INPUT_FOLDER="/mnt/c/Obsidian/BrainCave/d5 WDODelta/50-59 Programmeren en development/56 Generative AI en LLM/56.15 PromptsLibrary"
+# 
+# User parameters
+#
+# Location of input and output files
+# TODO: make us of ~ possible in setting of path
+FABRIC_PATTERNS_FOLDER="/home/jelle/.config/fabric/patterns"
+OBSIDIAN_OUTPUT_FOLDER="/mnt/c/Obsidian/BrainCave/Extra/textgenerator/templates/fabric"
+OBSIDIAN_INPUT_FOLDER="/mnt/c/Obsidian/BrainCave/d5 WDODelta/50-59 Programmeren en development/56 Generative AI en LLM/56.15 PromptsLibrary"
 
-    # Headings to extract from markdown files
-    BASE_WORDS = ['Identity', 'Purpose', 'Task', 'Goal']
+# Headings to extract from markdown files
+BASE_WORDS = ['Identity', 'Purpose', 'Task', 'Goal']
 
-    # Espanso parameters
-    DEFAULT_TRIGGER = ";;fab"
-    YAML_OUTPUT_FOLDER=f"/mnt/c/Users/{windows_user}/AppData/Roaming/espanso/match"
+# Espanso parameters
+DEFAULT_TRIGGER = ";;fab"
+YAML_OUTPUT_FOLDER=f"/mnt/c/Users/{windows_user}/AppData/Roaming/espanso/match"
 
 # Qdrant database parameters
 # TODO: deze paramater wordt nu niet in het script gebruikt, is nu hard coded, dit moet wel gebruikt worden
